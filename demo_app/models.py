71f5a9a8b09ab812a88bc38201d6014cf4d1977c
123abc958 <news@all.sakura.ne.jp>
2021-04-01 19:12:15 +0900

    deploy

--- demo_app/models.py
+++ demo_app/models.py
@@ -1,3 +1,23 @@
-from django.db import models
-
 # Create your models here.
+from __future__ import unicode_literals
+from django.db import models
+from django.utils import timezone 
+
+
+class Profile(models.Model):
+    name = models.CharField('名前', max_length=35)
+    education = models.CharField('教育', max_length=35)
+    github= models.CharField('GitHub', max_length=35)
+    facebook= models.CharField('facebook', max_length=35)
+    blog= models.CharField('blog', max_length=35)
+    mail= models.CharField('mail', max_length=35)
+    mobile = models.CharField('携帯', max_length=35)
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+   title = models.CharField('タイトル', max_length=35)
+   text = models.TextField('本文')
+   created_at = models.DateTimeField('投稿日', default=timezone.now)   
+   def __str__(self):
+       return self.title
--- demo_app/views.py
+++ demo_app/views.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Profile, Post
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-   return render(request, 'demo_app/index.html')
-
+  profile = Profile.objects.get(id=1)
+  return render(request, 'demo_app/index.html', {'profile': profile})
+ 
 def about(request):
    return render(request, 'demo_app/about.html')
+
+def post(request):
+   post=Post.objects.all().order_by('-id')
+   return render( request, 'demo_app/post.html', {'post':post})
+
